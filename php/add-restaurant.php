c2da8917e3dd417f67f77d36be20f6cc6b5000b1
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-06-22 20:58:56 +0700

    commit

--- php/add-restaurant.php
+++ php/add-restaurant.php
@@ -8,4 +8,4 @@ $longitude = doubleval($_POST["longitude"]);
 $address = $_POST["address"];
 $rating = $_POST["rating"];
 $sellerID = $_POST["seller_id"];
-$c->query("INSERT INTO restaurants (id, name, img_url, latitude, longitude, address, rating) VALUES ('" . $id . "', '" . $name . "', '" . $imgURL . "', " . $latitude . ", " . $longitude . ", '" . $address . "', '" . $rating . "', '" . $sellerID . "')");
+$c->query("INSERT INTO restaurants (id, name, img_url, latitude, longitude, address, rating, seller_id) VALUES ('" . $id . "', '" . $name . "', '" . $imgURL . "', " . $latitude . ", " . $longitude . ", '" . $address . "', '" . $rating . "', '" . $sellerID . "')");
