d9b424555358e4d203ab1cb067b70201966a43c5
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-06-22 20:44:46 +0700

    commit

---
+++ php/get-nearest-restaurants.php
@@ -0,0 +1,12 @@
+<?php
+include 'db.php';
+$latitude = $_POST["latitude"];
+$longitude = $_POST["longitude"];
+$results = $c->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $latitude . "), 2) + POW(69.1 * (" . $longitude . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM restaurants HAVING distance < 25 ORDER BY distance;");
+$restaurants = [];
+if ($results && $results->num_rows > 0) {
+	while ($row = $results->fetch_assoc()) {
+		array_push($restaurants, $row);
+	}
+}
+echo json_encode($restaurants);
