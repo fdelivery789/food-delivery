b060f8d86cbff2336b7826c66825c5d9db96d826
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-06-24 03:53:31 +0700

    commit

--- php/add-order.php
+++ php/add-order.php
@@ -9,3 +9,5 @@ $driverID = $_POST["driver_id"];
 $totalItems = intval($_POST["total_items"]);
 $totalPrice = intval($_POST["total_price"]);
 $c->query("INSERT INTO orders (buyer_address, buyer_id, fee, restaurant_id, seller_id, driver_id, total_items, total_price) VALUES ('" . $buyerAddress . "', '" . $buyerID . "', " . $fee . ", '" . $restaurantID . "', '" . $sellerID . "', '" . $driverID . "', " . $totalItems . ", " . $totalPrice . ")");
+$orderID = mysqli_insert_id($c);
+echo $orderID;
