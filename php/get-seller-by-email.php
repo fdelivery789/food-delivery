cc754f6effbca3d88db65c56a42e48026c778154
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-08-22 10:34:30 +0700

    commit

--- php/get-seller-by-email.php
+++ php/get-seller-by-email.php
@@ -1,7 +1,7 @@
 <?php
 include 'db.php';
 $email = $_POST["email"];
-$results = $c->query("SELECT * FROM users email='" . $email . "'");
+$results = $c->query("SELECT * FROM users WHERE email='" . $email . "'");
 if ($results && $results->num_rows > 0) {
 	$row = $results->fetch_assoc();
 	echo json_encode($row);
